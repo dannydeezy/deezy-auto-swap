f104eaf3fc15da86ca7da83d638347cab2b679b2
Danny Diekroeger <dannydiek@gmail.com>
2022-11-19 09:09:39 -0800

    catch lnd erroors

--- index.js
+++ index.js
@@ -64,7 +64,12 @@ async function selectChannels() {
 }
 
 async function getChainBalanceSats() {
-    const { utxos } = await getUtxos({ lnd, min_confirmations: 0 })
+    const { utxos } = await getUtxos({ lnd, min_confirmations: 0 }).catch(err => {
+        console.log('Error fetching utxos')
+        console.error(err)
+        return {}
+    })
+    if (!utxos) return null
     const utxoSumSats = utxos.reduce((acc, utxo) => acc + utxo.tokens, 0)
     return utxoSumSats
 }
@@ -170,8 +175,16 @@ async function run() {
 
     if (config.LN_ONCHAIN_TARGET_RATIO) {
         const chainBalanceSats = await getChainBalanceSats()
+        if (chainBalanceSats == null) return
+        
         console.log(`Chain balance is ${chainBalanceSats / 100000000} btc`)
-        const { channel_balance } = await getChannelBalance({ lnd })
+        const { channel_balance } = await getChannelBalance({ lnd }).catch(err => {
+            console.log('Error fetching channel balance')
+            console.error(err)
+            return {}
+        })
+        if (channel_balance == undefined) return
+
         console.log(`Channel balance is ${channel_balance / 100000000} btc`)
         const currentLnOnchainRatio = channel_balance / (channel_balance + chainBalanceSats)
         console.log(`Current LN/chain ratio is ${currentLnOnchainRatio}`)
